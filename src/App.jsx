2a761439a90c6e498cb0da6fbb6fecf18f56e5ab
김호준 <elddy0948@hotmail.co.kr>
2020-07-22 20:34:05 +0900

    1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { storiesOf } from "@storybook/react";
+import { render } from "@testing-library/react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>efl</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
