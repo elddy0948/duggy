5da7829f38c4c9e214b4d6686566aa00f2222266
junho0956 <junho0956@gmail.com>
2020-08-12 01:38:39 +0900

    album page

--- src/Screens/Album.js
+++ src/Screens/Album.js
@@ -12,16 +12,57 @@ class Album extends React.Component{
   render(){
     return (
       <div>
-        <h1>Duggy - My Story</h1>
-        <iframe 
-        width="560" 
-        height="315" 
-        src="https://www.youtube.com/embed/twGrfBQhfgE" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-        allowfullscreen>
-        </iframe>
-        <h1>hello world</h1>
+        <div class = "album_col_div"/>
+        <div id = "album_grid">
+        
+          <div id = "album_primary">
+            <div id = "album_primary_row1">
+              <div id = "album_primary_row_div"/>
+                <iframe 
+                width="728" 
+                height="410"
+                src="https://www.youtube.com/embed/twGrfBQhfgE" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+                </iframe>
+              <div id = "album_primary_row_div"/>
+            </div>
+            <div id = "album_primary_row2">
+              <div id = "album_primary_row2_title">
+                <div id = "album_primary_row_div"/>
+                <div id = "album_songName">%songName%</div>
+                <div id = "album_primary_row_div"/>
+              </div>
+              <div id = "album_primary_row2_discription">
+                <div id = "album_primary_row_div"/>
+                <div id = "album_discription">%discription%</div>
+                <div id = "album_primary_row_div"/>
+              </div>
+            </div>
+          </div>
+
+            <div id = "album_secondary">
+              
+              <div>
+                <table id = "album_songNameList">
+                  <tr id = "songNameTitle"><h5>album_Xsheet song List</h5></tr>
+                  <tr id = "songName">SongName</tr>
+                  <tr id = "songName">SongName</tr>
+                  <tr id = "songName">SongName</tr>
+                  <tr id = "songName">SongName</tr>
+                  <tr id = "songName">SongName</tr>
+                  <tr id = "songName">SongName</tr>
+                  <tr id = "songName">SongName</tr>
+                  <tr id = "songName">SongName</tr>
+                  <tr id = "songName">SongName</tr>
+                  <tr id = "songName">SongName</tr>
+                </table>
+              </div>
+              <div/>
+            </div>
+
+        </div>
       </div>
     )
   }
