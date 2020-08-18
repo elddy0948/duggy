2faa2ce2c72ed5215dd2239cd244d449905c3642
junho0956 <junho0956@gmail.com>
2020-08-19 00:37:01 +0900

    feature/album-paging

--- src/Screens/Album.js
+++ src/Screens/Album.js
@@ -3,6 +3,9 @@ import "../sass/materialize.scss";
 import firebase from "../firebase";
 import {firestore} from '../firebase';
 import '../App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Sheet_list from './Sheet_list';
+import Album_XSheet from './Album_XSheet';
 
 import jQuery from "jquery";
 import $ from "jquery";
@@ -23,29 +26,6 @@ class Album extends React.Component{
 
   // HELP ME ! i dont know method firestore read
   componentDidMount(){
-    firestore.collection(this.state.sheet).get().then(res => {
-      // this.setState({songNameList:res});
-      // 어떻게 첫번째 문서에 접근하냔 말입니다 행님들..
-      // 일단 전체 다받아와서 접근할테니 방법좀 구사해주십쇼!
-      var doclist = [];
-      var doclist2 = [];
-      var list = [];
-
-      res.forEach(doc => {
-        let songname = doc.get('songName');
-        let songurl = '';
-        songurl += doc.get('youtubeURL');
-        let real_songurl = songurl.replace("watch?v=", "embed/");
-
-        doclist.push(songname);
-        doclist2.push(real_songurl);
-        list.push(
-        <tr id = "songName" onClick = {() => this.page_change(songname, real_songurl)}>
-        {songname}</tr>)
-      })
-      this.setState({songNameList:list});
-      this.setState({songName:doclist[0], songUrl : doclist2[0]});
-    })
 
     $(document).on("click", "#songName", function(){
       $(this).parent().children().removeAttr("style");
@@ -54,66 +34,24 @@ class Album extends React.Component{
       $(this).css({background:'#bdbdbd'});
     });
 
-  }
+    // $(document).on("ready", ".tabs", function(){
+    //   $('.tabs').tabs({
+    //     swipeable : true
+    //   });
+    // })
 
-  page_change(name, url){
-    this.setState({songName:name, songUrl:url});
+    // $(document).ready(function(){
+    //   $(".tabs").tabs({
+    //     swipeable : true
+    //   })
+    // })
   }
 
   render(){
 
     return (
-      <div>
-        <div class = "album_col_div"/>
-        <div id = "album_grid">
-
-            <div id = "album_primary">
-              <div id = "album_primary_row1">
-                <div id = "album_primary_row_div"/>
-                  <iframe 
-                  width="728" 
-                  height="410"
-                  src={this.state.songUrl}
-                  frameborder="0" 
-                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-                  allowfullscreen>
-                  </iframe>
-                <div id = "album_primary_row_div"/>
-              </div>
-              <div id = "album_primary_row2">
-                <div id = "album_primary_row2_title">
-                  <div id = "album_primary_row_div"/>
-                  <div id = "album_primary_songName">{this.state.songName}</div>
-                  <div id = "album_primary_row_div"/>
-                </div>
-                <div id = "album_primary_row2_discription">
-                  <div id = "album_primary_row_div"/>
-                  <div id = "album_primary_discription">{this.state.discription}</div>
-                  <div id = "album_primary_row_div"/>
-                </div>
-              </div>
-            </div>
-
-            <div id = "album_secondary">
-
-              <div id = "album_secondary_title_col">
-              <h5 id = "songNameTitle">album_{this.state.sheet} song List</h5><div/>
-              </div>
-
-              <div id = "album_secondary_col">
-                
-                <div id = "album_songNameList_table">
-                  <table id = "album_songNameList">
-                    {this.state.songNameList}
-                  </table>
-                </div>
-                <div/>
-
-              </div>
-
-            </div>
-
-        </div>
+      <div id = "album_top">
+        <Sheet_list/>
       </div>
     )
   }
--- src/Screens/Sheet_list.js
+++ src/Screens/Sheet_list.js
@@ -2,37 +2,77 @@ import React from 'react';
 import ReactDom from 'react-router-dom';
 import firebase from '../firebase';
 import {firestore} from '../firebase';
+import "../sass/materialize.scss";
+import '../App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Album_XSheet from './Album_XSheet';
+
+import jQuery from "jquery";
+import $ from "jquery";
+window.$ = window.jQuery = jQuery;
 
 class Sheet_list extends React.Component{
 
     constructor(props){
         super(props);
         this.state = {
-            SheetList : []
+            SheetList : [],
+            XSheetList : [],
         }
     }
 
     componentDidMount = () => {
         var list = [];
+        var xsheet_list = [];
         var sheetlist = [];
+        
         firestore.collection('SheetList').get()
         .then(res=>{
             res.forEach(doc => {
                 list = doc.get('list');
                 var i;
                 for(i = 0; i<list.length; i++){
-                var url = "/album-";
+                var url = "#album-";
                 url += list[i];
-                sheetlist.push(<li class = "li1"><a href={url}>{list[i]}</a></li>);
+                var url2 = "album-";
+                url2 += list[i];
+
+                sheetlist.push(<li class = "tab col s2"><a class = "black-text" href={url}>{list[i]}</a></li>);
+                xsheet_list.push(<div id = {url2} class = "col s12"><Album_XSheet url = {list[i]}/></div>)
                 }
+
                 this.setState({SheetList : sheetlist});
+                this.setState({XSheetList : xsheet_list});
             })
+
+            $(document).ready(function(){
+                $(".tabs").tabs({
+                    swipeable : true
+                })
+                $('#change_height').children().eq(1).css('height','650px');
+                $('#tabs-swipe-demo').css('padding-top','0%');
+            })
+
         })
+
+        // $(document).on("ready", function(){
+        //     $('.tabs').tabs({
+        //       swipeable : true
+        //     });
+        //   })
+
     }
 
     render(){
         return(
-            <ul class = "ul1">{this.state.SheetList}</ul>
+            <div class = "row">
+                <div class = "col s12" id = "change_height">
+                    <ul id = "tabs-swipe-demo" class = "tabs">
+                        {this.state.SheetList}
+                    </ul>
+                    {this.state.XSheetList}
+                </div>
+            </div>
         )
     }
 }
