c4a78c0862351354ecc93ee8d061abb86f0e37c0
Hojoon Kim <elddy0948@hotmail.co.kr>
2020-08-31 00:18:27 +0900

    삭제버튼 아직 구현 안함!

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -3,55 +3,53 @@ import ReactDom from "react-dom";
 
 import "../sass/materialize.scss";
 import "../App.css";
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
-import {firestore} from '../firebase';
-import {auth} from '../firebase';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import { firestore } from "../firebase";
+import { auth } from "../firebase";
 
-import {signInWithGoogle} from '../firebase';
-import {signInWithFirebase} from '../firebase';
+import { signInWithGoogle } from "../firebase";
+import { signInWithFirebase } from "../firebase";
 
-import KaKaoLogin from 'react-kakao-login';
-import Kakao from 'kakaojs';
+import KaKaoLogin from "react-kakao-login";
+import Kakao from "kakaojs";
 
 const url = "https://duggy-music.firebaseio.com";
 
-class Login extends React.Component{
-
-  constructor(props){
+class Login extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      kakao : null
+      kakao: null,
     };
   }
 
-  componentDidMount(){
-    if(Kakao.Auth == null){
+  componentDidMount() {
+    if (Kakao.Auth == null) {
       Kakao.init(process.env.REACT_APP_KAKAO_API_KEY);
     }
-    this.setState({kakao : Kakao});
+    this.setState({ kakao: Kakao });
   }
 
-  handler = e => {
-    if(firebase.auth().currentUser){
+  handler = (e) => {
+    if (firebase.auth().currentUser) {
       alert("로그인 세션 유지중 -> 자동 로그아웃");
       firebase.auth().signOut();
-    }
-    else {
-      var email = document.getElementById('email').value;
-      var password = document.getElementById('password').value;
+    } else {
+      var email = document.getElementById("email").value;
+      var password = document.getElementById("password").value;
       if (email.length < 4) {
-        alert('Please enter an email address.');
+        alert("Please enter an email address.");
         return;
       }
       if (password.length < 4) {
-        alert('Please enter a password.');
+        alert("Please enter a password.");
         return;
       }
-      
+
       // signInWithFirebase(email, password)
       // .then(()=>{
       //   this.props.history.push("/");
@@ -60,68 +58,75 @@ class Login extends React.Component{
       //   alert(error);
       //   return;
       // });
-  
-      firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-      .then(()=>{
-        signInWithFirebase(email, password)
-        .then(()=>{
-          this.props.history.push("/");
-        })
-        .catch(error => {
-          alert(error);
-          return;
-        });
-      })
 
-    }
-    // document.getElementById('quickstart-sign-in').disabled = true;
-  }
-
-  handler_google = e => {
-    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-    .then(()=>{
-      signInWithGoogle()
-      .then(()=>{
-
-        // 첫 로그인인 경우 authentication 에는 등록되지만 firestore에는 들어가지 않음
-        // 그러므로 구글로 로그인하는 경우 추가적으로 firestore에 정보를 입력할 필요가 있음
-        var login_check = false;
-        firestore.collection("Users").get()
-        .then(res => {
-          res.forEach(doc => {
-            if(doc.get("Email") === auth.currentUser.email){
-              login_check = true;
-              return true;
-            }
-          })
-
-          if(login_check == false){
-            var store_data = {
-              Email : auth.currentUser.email,
-              authority : [],
-              name : auth.currentUser.displayName
-            };
-
-            firestore.collection("Users").doc().set(store_data)
-            .then( () => {
+      firebase
+        .auth()
+        .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+        .then(() => {
+          signInWithFirebase(email, password)
+            .then(() => {
               this.props.history.push("/");
             })
-            .catch( error => {
+            .catch((error) => {
               alert(error);
               return;
-            })
-          }
-        })
+            });
+        });
+    }
+    // document.getElementById('quickstart-sign-in').disabled = true;
+  };
 
-        this.props.history.push("/");
-      })
-      .catch(error => {
-        // 배포할 때 수정
-        alert(error);
-        return;
+  handler_google = (e) => {
+    firebase
+      .auth()
+      .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+      .then(() => {
+        signInWithGoogle()
+          .then(() => {
+            // 첫 로그인인 경우 authentication 에는 등록되지만 firestore에는 들어가지 않음
+            // 그러므로 구글로 로그인하는 경우 추가적으로 firestore에 정보를 입력할 필요가 있음
+            var login_check = false;
+            firestore
+              .collection("Users")
+              .get()
+              .then((res) => {
+                res.forEach((doc) => {
+                  if (doc.get("Email") === auth.currentUser.email) {
+                    login_check = true;
+                    return true;
+                  }
+                });
+
+                if (login_check == false) {
+                  var store_data = {
+                    Email: auth.currentUser.email,
+                    authority: [],
+                    name: auth.currentUser.displayName,
+                  };
+
+                  firestore
+                    .collection("Users")
+                    .doc()
+                    .set(store_data)
+                    .then(() => {
+                      this.props.history.push("/");
+                    })
+                    .catch((error) => {
+                      alert(error);
+                      return;
+                    });
+                }
+              });
+
+            this.props.history.push("/");
+          })
+          .catch((error) => {
+            // 배포할 때 수정
+            alert(error);
+            return;
+          });
       });
-    })
-  }
+  };
 
   handler_kakao = () => {
     // 작업순서
@@ -131,90 +136,97 @@ class Login extends React.Component{
     // 4. 사용자의 정보를 성공적으로 받았다면, Firebase Admin SDK 을 이용해서 firebase Auth 에 User을 생성한다
     // 5. 생성된 User의 UID를 통해 Firebase Custom Token을 생성해서 클라이언트에게 반환한다
     // 6. Firebase Auth 에서 제공하는 signinWithcustomtoken 메서드의 인자로 Custom Token을 넘겨 로그인을 처리한다
-
     // this.state.kakao.Auth.login({
     //   success : (reponse) => {
     //     alert(response);
     //   }
     // })
-  }
+  };
 
   forget_user_password = () => {
     this.props.history.push("/password_reset");
-  }
-  
-  render(){
+  };
+
+  render() {
     return (
-      <body id = "login_body">
+      <body id="login_body">
         <div class="container">
-          <div class="row"/>
+          <div class="row" />
           <div class="row">
-
-          <h4 id = "signinTitle">Sign In</h4>
-              <div class="row">
-                <div class="col s3" />
-                <div class="input-field col s6">
-                  <label for = "email">UserEmail</label>
-                  <input 
-                    // placeholder="example@google.com" 
-                    id="email" 
-                    type="email"
-                    class="validate"
-                    />
-                </div>
-                <div class="col s3" />
+            <h4 id="signinTitle">Sign In</h4>
+            <div class="row">
+              <div class="col s3" />
+              <div class="input-field col s6">
+                <label for="email">UserEmail</label>
+                <input
+                  // placeholder="example@google.com"
+                  id="email"
+                  type="email"
+                  class="validate"
+                />
               </div>
-              
-              <div class="row">
-                <div class="col s3" />
-                <div class="input-field col s6">
-                  <label for = "password">password</label>
-                  <input 
-                    // placeholder="Password" 
-                    id="password"
-                    type="password" 
-                    class="validate"
-                    />
-                  <a href = "#" onClick = {this.forget_user_password}>forgot password?</a>
-                </div>
-                <div class="col s3" />
-              </div>
-              
-              <div class="row">
-                <div class="col s4" />
-                <div class="col s4">
-                  <button
-                    class="waves-effect waves-light btn-large col s12"
-                    onClick = {this.handler} > Sign In </button>
-                </div>
-                <div class="col s4" />
-              </div>
+              <div class="col s3" />
+            </div>
 
             <div class="row">
-                <div class="col s4"/>
-                <div class="col s4">
+              <div class="col s3" />
+              <div class="input-field col s6">
+                <label for="password">password</label>
+                <input
+                  // placeholder="Password"
+                  id="password"
+                  type="password"
+                  class="validate"
+                />
+                <a href="#" onClick={this.forget_user_password}>
+                  forgot password?
+                </a>
+              </div>
+              <div class="col s3" />
+            </div>
+
+            <div class="row">
+              <div class="col s4" />
+              <div class="col s4">
                 <button
-                    class="waves-effect waves-light btn-large col s12"
-                    onClick = {this.handler_google} > sign in with Google </button>
-                </div>
-                <div class="col s4"/>
+                  class="waves-effect waves-light btn-large col s12"
+                  onClick={this.handler}
+                >
+                  {" "}
+                  Sign In{" "}
+                </button>
+              </div>
+              <div class="col s4" />
             </div>
 
             <div class="row">
-                <div class="col s4"/>
-                <div class="col s4">
-                <KaKaoButton
-                    jsKey={process.env.REACT_APP_KAKAO_API_KEY}
-                    // class="waves-effect waves-light btn-large col s12"
-                    buttonText = "sign in with KaKao"
-                    onSuccess = {this.responseKaKao}
-                    onFailure = {this.responseError}
-                    />
-                    {/* onClick = {this.handler_kakao} >sign in with KaKao</KaKaoButton> */}
-                </div>
-                <div class="col s4"/>
+              <div class="col s4" />
+              <div class="col s4">
+                <button
+                  class="waves-effect waves-light btn-large col s12"
+                  onClick={this.handler_google}
+                >
+                  {" "}
+                  sign in with Google{" "}
+                </button>
+              </div>
+              <div class="col s4" />
             </div>
 
+            <div class="row">
+              <div class="col s4" />
+              <div class="col s4">
+                <KaKaoButton
+                  jsKey={process.env.REACT_APP_KAKAO_API_KEY}
+                  // class="waves-effect waves-light btn-large col s12"
+                  buttonText="sign in with KaKao"
+                  onSuccess={this.responseKaKao}
+                  onFailure={this.responseError}
+                />
+                {/* onClick = {this.handler_kakao} >sign in with KaKao</KaKaoButton> */}
+              </div>
+              <div class="col s4" />
+            </div>
           </div>
         </div>
       </body>
@@ -228,16 +240,16 @@ const KaKaoButton = styled(KaKaoLogin)`
   height: 100%;
   line-height: 44px;
   color: #783c00;
-  background-color: #FFEB00;
+  background-color: #ffeb00;
   border: 1px solid transparent;
   border-radius: 3px;
   font-size: 16px;
   font-weight: bold;
   text-align: center;
   cursor: pointer;
-  $:hover{
-    box-shadow: 0 0px 15px 0 rgba(0,0,0,0.2)
+  $:hover {
+    box-shadow: 0 0px 15px 0 rgba(0, 0, 0, 0.2);
   }
-`
+`;
 
 export default Login;
